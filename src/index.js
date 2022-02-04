ec796f85894d48fd1494f759b9015d43a6ce4fc3
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2022-02-04 15:03:04 +0300

    easy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = ['(', '[', '{', '|', '1', '3', '5', '7', '8',]
+  const brackets = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+    '|': '|',
+    '2': '1',
+    '4': '3',
+    '6': '5',
+    '7': '7',
+    '8': '8',
+
+  }
+
+  let steck = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    openBrackets.forEach(element => {
+      if (currentSymbol === element)
+        steck.push(currentSymbol);
+    });
+    let topElement = steck[steck.length - 1];
+    if (brackets[currentSymbol] === topElement) {
+      steck.pop();
+    }
+  }
+  return steck.length === 0
 }
